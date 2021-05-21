7ee0ffe3f704187dce5f25038fbdc749a3dcb335
jerismith32 <jerismith32@gmail.com>
2021-05-21 18:44:35 -0400

    updated variables in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var selectedCityEl = document.querySelector("#searchedCity")
+var selectedCityInfoEl = document.querySelector("#selectedCityResults");
+var citySearchEl = document.querySelector("#selectedCity");
+var searchEl = document.querySelector("#search");
+var forcastCardsEl = document.querySelector("#5dayForcastCards");
+var previousCitiesEl = document.querySelector("#saveCities");
