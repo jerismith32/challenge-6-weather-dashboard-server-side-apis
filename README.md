fb1c1feb6e36b7b4ea6fff3d506e135d71c71e92
jerismith32 <jerismith32@gmail.com>
2021-05-21 13:28:48 -0400

    updated header

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 https://getbootstrap.com/docs/5.0/customize/color/
 Link for color guide in Bootstrap
+
+
+https://getbootstrap.com/docs/4.0/components/navbar/
+Navbar
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,4 @@
 var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city");
+var currentDayWeatherContainerEl = document.querySelector("#currentDayWeather");
+var searchCity = document.querySelector("#search-response");
