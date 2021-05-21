328a26229b01c93ebd63709dfff7b2735cf93bad
jerismith32 <jerismith32@gmail.com>
2021-05-21 18:34:28 -0400

    reformatted and updated html

--- assets/js/script.js
+++
@@ -1,4 +0,0 @@
-var userFormEl = document.querySelector("#user-form");
-var cityInputEl = document.querySelector("#city");
-var currentDayWeatherContainerEl = document.querySelector("#currentDayWeather");
-var searchCity = document.querySelector("#search-response");
